4894256aeacb96921324f4567484d236719a72b7
Sri Renga <srirenga1601@gmail.com>
2018-09-15 18:36:04 +0530

    fx

--- jboss/manifests/install.pp
+++ jboss/manifests/install.pp
@@ -8,7 +8,7 @@ class jboss::install (
   file { $java:
     ensure => file,
     mode => '0777',
-    cwd => $temp_dir,
+    path => $temp_dir,
     source => "puppet:///modules/jboss/${java}"
   }->
   exec { 'install java JDK':
@@ -19,7 +19,7 @@ class jboss::install (
   file { '$jboss_package':
     ensure => file,
     mode => '0644',
-    cwd => $temp_dir,
+    path => $temp_dir,
     source => "puppet:///modules/jboss/${jboss_package}"
   }->
   exec { 'extract jboss':
