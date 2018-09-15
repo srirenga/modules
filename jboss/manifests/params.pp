09b1130452eadfb8081b8f9a847d0e223ce63971
Sri Renga <srirenga1601@gmail.com>
2018-09-15 18:23:23 +0530

    fx

--- jboss/manifests/params.pp
+++ jboss/manifests/params.pp
@@ -2,6 +2,6 @@
 #
 class jboss::params {
   $java = 'jdk-10.0.2_linux-x64_bin.rpm'
-  $temp_dir = /tmp/
-  $jboss_package = wildfly-14.0.1.Final-src.tar.gz
+  $temp_dir = '/tmp/'
+  $jboss_package = 'wildfly-14.0.1.Final-src.tar.gz'
 }
